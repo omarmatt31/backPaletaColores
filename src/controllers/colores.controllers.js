3e0271d67a262a15d2211936c57840127ae7ff01
Omar Mattos <omarmatt84@gmail.com>
2025-08-31 12:42:16 -0300

    podemos ver los datos que llegan en la solicitud para crear un color

--- src/controllers/colores.controllers.js
+++ src/controllers/colores.controllers.js
@@ -10,5 +10,13 @@ export const leerColores =async(req, res)=>{
 }
 
 export const crearColor = async (req, res)=>{
-
+    try{
+        console.log(req.body);
+        //const nuevoColor = new Color(req.body);
+        //await nuevoColor.save();
+        //res.status(201).json({mensaje: 'El color fue creado exitosamente'})
+    }catch(error){
+        console.error(error)
+        res.status(500).json({mensaje: 'Error al crear el color'})
+    }
 }
